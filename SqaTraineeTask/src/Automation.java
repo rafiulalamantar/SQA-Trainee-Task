858aa3d8baaad1560772932476e74e5bd3795da7
Rafiul Alam Antar <rafiulantar@gmail.com>
2021-11-13 13:45:22 +0600

    visit the given url

--- SqaTraineeTask/src/Automation.java
+++ SqaTraineeTask/src/Automation.java
@@ -1,9 +1,13 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Automation {
 
 	public static void main(String[] args) {
 		
-		System.setProperty("webdriver.chrome.driver","E:\\Chrome Driver\\chromedriver_win32\\chromedriver.exe");
+		System.setProperty("webdriver.chrome.driver","G:\\Software SQA\\chromedriver_win32\\chromedriver.exe");
+		WebDriver driver=new ChromeDriver();
+		driver.get("http://www.phptravels.net/");
 
 	}
 
