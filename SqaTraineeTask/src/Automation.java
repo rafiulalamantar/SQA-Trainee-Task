bf8bda9456841c530b4806b3e308bdbd52edf512
Rafiul Alam Antar <rafiulantar@gmail.com>
2021-11-13 13:38:11 +0600

    new class added and selenium added with maven

---
+++ SqaTraineeTask/src/Automation.java
@@ -0,0 +1,10 @@
+
+public class Automation {
+
+	public static void main(String[] args) {
+		
+		System.setProperty("webdriver.chrome.driver","E:\\Chrome Driver\\chromedriver_win32\\chromedriver.exe");
+
+	}
+
+}
