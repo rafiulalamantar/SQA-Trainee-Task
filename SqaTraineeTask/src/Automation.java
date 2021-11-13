fefcc898370a69e344383d1c2b131c2dde91deb1
Rafiul Alam Antar <rafiulantar@gmail.com>
2021-11-13 13:53:42 +0600

    select tour from search bar

--- SqaTraineeTask/src/Automation.java
+++ SqaTraineeTask/src/Automation.java
@@ -1,3 +1,4 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -8,6 +9,7 @@ public class Automation {
 		System.setProperty("webdriver.chrome.driver","G:\\Software SQA\\chromedriver_win32\\chromedriver.exe");
 		WebDriver driver=new ChromeDriver();
 		driver.get("http://www.phptravels.net/");
+		driver.findElement(By.xpath("//li[@data-position=\"3\"]")).click();
 
 	}
 
